61ed49c228441ab53054d05e190ad7968dfd5285
Nicole W <Nicole.Wilkins2018@gmail.com>
2018-10-28 11:29:03 -0400

    spotify works

--- key.js
+++ key.js
@@ -1,11 +1,6 @@
-var Spotify = new Spotify(keys.spotify);
+var spotifyKeys = {
+    ID:process.env.SPOTIFY_ID,
+    SECRET:process.env.SPOTIFY_SECRET
+};
 
-//needed for reading and writing files
-var fs = require("fs");
-
-console.log('this is loaded');
-
-exports.Spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-}
+module.exports = spotifyKeys;
--- liri.js
+++ liri.js
@@ -1,35 +1,76 @@
 //homework instructions said to put this
 require("dotenv").config();
-
+var SPOTIFY_API = require('./key.js');
 //needed to use spotify npm
-var spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
 //needed for reading and writing files
 var fs = require("fs");
 
+var spotify = new Spotify({
+    id: SPOTIFY_API.ID,
+    secret: SPOTIFY_API.SECRET
+  });
+
 //to read key.js file
-fs.readFile("key.js", "utf8", function(error, data) {
-    if(error) {
-        return console.log(error)
-    }
-})
+// fs.readFile("key.js", "utf8", function(error, data) {
+//     if(error) {
+//         return console.log(error)
+//     }
+// })
 
 //concert-this
 
 //spotify-this-song
-function spotifyThisSong() {
-    if (song === undefined) {
-        song = "The Sign"
-    };
-};
+
+//spotify search from docs
+// search: function({ type: 'artist OR album OR track', query: 'My search query', limit: 20 }, callback);
+// function spotifyThisSong() {
+//     if (song === undefined) {
+//         song = "The Sign"
+//     };
+// };
+function spotifyThisSong(query='The Sign ace of base'){
 
 //from docs
-spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
+spotify.search({ type: 'track', query: query, limit:1 }, function(err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
     }
    
-  console.log(data); 
+  console.log('Artists Name: ' + data.tracks.items[0].album.artists[0].name); 
+  console.log('Song name: ' + data.tracks.items[0].name); 
+  console.log('Album name: ' + data.tracks.items[0].album.name); 
+  console.log('Popularity: ' + data.tracks.items[0].popularity); 
+  
   });
+}
+//This runs whole app
+var command = process.argv[2];
+var option = process.argv[3] || null;
+commandHandler(command, option);
+function commandHandler(argvCommand, argvOption) {
+    if (argvCommand === 'spotify-this-song') {
+        if(argvOption) {
+       spotifyThisSong(argvOption);
+       return;
+        }
+        spotifyThisSong();
+    }
+    else if (argvCommand === 'deposit') {
+        console.log('display deposit')
+      
+    }
+    else if (argvCommand === 'withdrawl') {
+        console.log('display withdrawl')
+        
+    }
+    else if (argvCommand === 'lotto') {
+        console.log('display lotto')
+    }
+    else { 
+        console.log('ERROR: enter either total, deposit, withdral, or lotto to contine')
+    }
+}
 
 //movie-this
 
